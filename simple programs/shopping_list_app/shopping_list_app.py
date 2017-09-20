955b5e3a31be18eae247b06367648d4c0c2117b2
annec22 <annecalija@gmail.com>
2017-09-19 18:35:51 -0700

    added functionality HELP, SHOW, and moved them as functions for shopping_list_app.py

--- simple programs/shopping_list_app/shopping_list_app.py
+++ simple programs/shopping_list_app/shopping_list_app.py
@@ -1,29 +1,46 @@
 # Filename: shopping_list_app.py
 # Versions
 # A simple python programming app that showcases use of:
-#      - list
-#      - loops   
-#      - user input
-# What it does:
-#      - Takes all that items you want to buy and stores in a list.
-#      - Once list is done, "DONE" should be entered to quit the app
-#      - Once quit is triggered by user, it displays all the items on the list
+#	- lists
+#	- loops
+#	- functions
+#	- user input
+# Features:
+#	- add items
+#	- show items
+#	- exit
+#	- show help
 
 print("Hi! Welcome to this simple shopping list app.")
-print("Please enter 'DONE' to exit. Happy shopping!")
+print("Type 'HELP' and <enter> for more options.")
+print("Start listing now...")
+
 item_list = []
 
+def show_list_items():
+	print("Here's your list: ")
+	print("="*30)
+	for item in item_list:
+		print("   - " + item)
+	print("="*30)
+	
+def show_help():
+	print(("="*11) + "HELP" + ("="*11))
+	print("You may type the following and press <enter>: ")
+	print("'HELP' - show help menu")
+	print("'SHOW' - show current list")
+	print("'DONE' - save list and exit app")
+	print("="*30)
+
 while True:
-  user_input = input("Enter item: ")
+	user_input = input("	>")
   
-  if user_input == "DONE":
-    break;
-  
-  item_list.append(user_input)
-  
-print("Here's your shopping list: ")
-print("="*20)
-for item in item_list:
-  print("  >>" + item)
-  
-print("="*20)
+	if user_input == "DONE":
+		show_list_items()
+		break
+	elif user_input == "SHOW":
+		show_list_items()
+	elif user_input == "HELP":
+		show_help()
+	else:
+		item_list.append(user_input)
